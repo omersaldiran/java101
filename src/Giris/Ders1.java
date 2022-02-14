337d570cdc9e2d0ceb8b8c271b3571d854845c72
omersaldiran <osaldiran@gmail.com>
2022-02-14 20:58:57 +0300

    Proje tamamlandı

--- src/Giris/Ders1.java
+++ src/Giris/Ders1.java
@@ -1,7 +1,36 @@
 package Giris;
 
+import java.util.Scanner;
+
 public class Ders1 {
     public static void main(String[] args) {
-        System.out.print("Merhaba");
+        System.out.print("Ortalama Hesaplama Programı\n");
+
+        int Matematik, Fizik, Kimya, Turkce, Tarih, Muzik;
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Fizik notunuzu girin : ");
+        Fizik = input.nextInt();
+
+        System.out.println("Kimya notunuzu girin : ");
+        Kimya = input.nextInt();
+
+        System.out.println("Turkce notunuzu girin : ");
+        Turkce = input.nextInt();
+
+        System.out.println("Tarih notunuzu girin : ");
+        Tarih = input.nextInt();
+
+        System.out.println("Muzik notunuzu girin : ");
+        Muzik = input.nextInt();
+
+        System.out.println("Metamatik notunuzu girin : ");
+        Matematik = input.nextInt();
+
+        double ortalama = (Matematik+Fizik+Kimya+Turkce+Tarih+Muzik)/6.0;
+        System.out.println("Ortalamanız : " + ortalama);
+        String durum = ortalama > 60 ? "Sınıfı Geçti" : "Sınıfta Kaldı";
+        System.out.println(durum);
+
     }
 }
