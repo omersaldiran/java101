cf2211a6f3d4b131384537e9ff5decffe34ce2eb
omersaldiran <osaldiran@gmail.com>
2022-05-29 18:15:04 +0300

    Generic metod çalışması eklendi

--- src/Generic/main.java
+++ src/Generic/main.java
@@ -2,7 +2,17 @@ package Generic;
 
 public class main {
     public static void main(String[] args) {
-        Nullable<String> a = new Nullable<>(null);
-        a.run();
+        //Nullable<String> a = new Nullable<>(null);
+        //a.run();
+        String[] arr1 = {"Merhaba","Dünya" };
+        Integer[] arr2 = {1,2,3,4,5};
+        printArr(arr1);
+        printArr(arr2);
+    }
+
+    public static <T> void printArr(T[] arr){
+        for(T a:arr){
+            System.out.println(a);
+        }
     }
 }
